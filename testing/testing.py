34abf1e7d7d7d2c9ba4c3415edb476573f04553c
Suryansh <tensorprogrammer@gmail.com>
2025-07-07 11:54:07 +0530

    removed unused file or folders

--- testing/testing.py
+++
@@ -1,8 +0,0 @@
-from codetraverse.main import create_fdep_data
-
-create_fdep_data(
-    root_dir="/Users/suryansh.s/Tree-sitter/hyperswitch",
-    output_base="fdep",
-    graph_dir="graph",
-    clear_existing=True
-)
