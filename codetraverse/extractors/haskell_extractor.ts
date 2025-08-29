5321b06417580e8b763e2050a40c578989104c7e
pramod.p@juspay.in <pramod.p@juspay.in>
2025-08-29 13:05:23 +0530

    merge ast with latest

--- codetraverse/extractors/haskell_extractor.ts
+++ codetraverse/extractors/haskell_extractor.ts
@@ -74,7 +74,6 @@ export class HaskellComponentExtractor implements ComponentExtractor {
     try {
       tree = this.parser.parse(fileContent);
     } catch (error) {
-      console.error(`Failed to parse file ${filePath}:`, error);
       return;
     }
 
