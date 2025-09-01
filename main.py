92f1cecbab1594009bed6b860a5a906c0cc76d2d
JignyasAnand <jignyas.ml@gmail.com>
2025-09-01 12:51:10 +0530

    remove py files, update package.json and tsconfig.json

--- main.py
+++
@@ -1,5 +0,0 @@
-from codetraverse import blackbox
-def main():
-    blackbox.main()
-if __name__ == "__main__":
-    main()
--- sample_code_repo_test/javascript/types.js
+++
@@ -1,3 +0,0 @@
-export default function type_func() {
-  return "chain complete";
-}
