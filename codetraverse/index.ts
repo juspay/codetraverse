9c4c7da59d47d6ee6627b66afb036a933b8b4469
Suryansh <tensorprogrammer@gmail.com>
2025-09-08 13:14:03 +0530

    exported extractComponentsFromFile in index.ts

--- codetraverse/index.ts
+++ codetraverse/index.ts
@@ -2,4 +2,4 @@ export { createFdepData, LANGUAGES_SUPPORTED } from './main';
 export { loadGraph } from './path';
 export type { Component } from './types/types';
 export { getAllModules, getModuleInfo, getFunctionInfo, getFunctionChildren, getFunctionParent, getSubgraph, getCommonParents, getCommonChildren, getImportantNodes, getNeighbors, findPath } from './utils/blackbox';
-export { runAstDiffFromConfig } from './utils/AstDifferOrchestrator';
+export { runAstDiffFromConfig, extractComponentsFromFile } from './utils/AstDifferOrchestrator';
